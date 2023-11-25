a61fb8133db5c96c100bad1b61a7b04b3177907b
EkeMinusYou <euonymuslke@gmail.com>
2023-11-25 17:13:10 +0900

    add index

--- webapp/sql/custom_isudns.sql
+++ webapp/sql/custom_isudns.sql
@@ -1,3 +1,5 @@
 USE isudns;
 DROP INDEX idx_name_disabled_domain ON records;
 CREATE INDEX idx_name_disabled_domain ON records(name, disabled, domain_id);
+DROP INDEX idx_domainmetadata_domain_id ON domainmetadata;
+CREATE INDEX idx_domainmetadata_domain_id ON domainmetadata(domain_id);
